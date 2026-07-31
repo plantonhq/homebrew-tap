1ed775a3e562a6f593a775c09a67ae3fe7b57fb1
Swarup Donepudi <swarup.donepudi@gmail.com>
2026-07-31 12:19:16 +0530

    chore: update planton-desktop to v0.0.34-desktop.20260731.0 (laptop release)

--- Casks/planton-desktop.rb
+++ Casks/planton-desktop.rb
@@ -1,6 +1,6 @@
 cask "planton-desktop" do
-  version "v0.0.34-desktop.20260727.2"
-  sha256 "1da699ac404e48f83c79dc854bb66c44815efea2ab7c6621b81558c25a70e8e8"
+  version "v0.0.34-desktop.20260731.0"
+  sha256 "8aee55c8d31061d067bb28a93f4d920e59c651dfa4ea4e239472d0b6ec646d63"
 
   url "https://downloads.planton.app/desktop/#{version}/planton-desktop-#{version}-universal-macos.dmg"
   name "Planton"
